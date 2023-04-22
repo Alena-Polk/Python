32379f2a6e49d8c67181acee99b0ae8581ea41d4
Alena-Polk <alenushkapolkina@yandex.ru>
2023-04-22 18:52:31 +0300

    Добавлена папка dz.36

--- main.py
+++ main.py
@@ -7320,7 +7320,7 @@
 # *.db, *.sqlite, *.sqlite3, *.sdb, *.db2
 
 
-import sqlite3
+# import sqlite3
 #
 # # con = sqlite3.connect('profile.db')
 # # cur = con.cursor()
@@ -7388,7 +7388,66 @@ import sqlite3
 #     for res in cur:
 #         print(res)
 
-    # res = cur.fetchone()
-    # print(res)
-    # res2 = cur.fetchmany(3)
-    # print(res2)
+# res = cur.fetchone()
+# print(res)
+# res2 = cur.fetchmany(3)
+# print(res2)
+
+# import sqlite3
+#
+# cars = [('BMW', 54000),
+#         ('Chevrolet', 46000),
+#         ('Daewoo', 38000),
+#         ('Citroen', 29000),
+#         ('Honda', 33000),
+#         ]
+# with sqlite3.connect('cars.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS CARS(
+#     car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     model TEXT,
+#     price INTEGER)
+#     """)
+#     cur.executescript("""
+#         DELETE FROM cars WHERE model LIKE 'B%':
+#         UPDATE cars SET price = price + 100;
+#     """)
+# cur.execute('UPDATE cars SET price = 0 WHERE model LIKE "B%"', {'Price': 0})
+
+# cur.executemany('INSERT INTO cars VALUES(NULL, ?, ?)', cars)
+
+# for car in cars:
+#     cur.execute('INSERT INTO cars VALUES(NULL, ?, ?)', car)
+# cur.execute('INSERT INTO cars VALUES(1, "Renault", 22000')
+# cur.execute('INSERT INTO cars VALUES(2, "Renault", 29000')
+# cur.execute('INSERT INTO cars VALUES(3, "Renault", 57000')
+# cur.execute('INSERT INTO cars VALUES(4, "Bentley", 35000')
+# cur.execute('INSERT INTO cars VALUES(5, "Audi", 52000')
+
+# con.commit()
+# con.close()
+
+import sqlite3
+
+con = None
+try:
+    con = sqlite3.connect('cars.db')
+    cur = con.cursor()
+    cur.executescript("""
+    CREATE TABLE IF NOT EXISTS CARS(
+     car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+     model TEXT,
+    price INTEGER);
+    BEGIN;
+    INSERT INTO cars VALUES(NULL, "Renault", 22000);
+    UPDATE cars SET price = price + 100;
+    """)
+    con.commit()
+except sqlite3.Error as e:
+    if con:
+        con.rollback()
+    print("Ошибка выполнения запроса", e)
+finally:
+    if con:
+        con.close()
