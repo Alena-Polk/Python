e0f1cd61293f6e9f133c4233c700e71e479792a6
Alena-Polk <alenushkapolkina@yandex.ru>
2023-04-30 14:00:15 +0300

    Добавлена папка dz.38

--- main.py
+++ main.py
@@ -7596,3 +7596,89 @@ import csv
 #     db_is_created = os.path.exists(DATABASE_NAME)
 #     if not db_is_created:
 #         db_creator.create_database()
+
+from jinja2 import Template
+
+# name = "Игорь"
+# age = 25
+# per = {'name': 'Игорь', 'age': 25}
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_age(self):
+#         return self.age
+#
+#
+# per = Person('Игорь', 25)
+# # tm = Template("Мне {{ a*2 }} лет. Меня зовут {{ n.upper() }}.")
+# tm = Template("Мне {{ p.age }} лет. Меня зовут {{ p.name}}.")
+# # tm = Template("Мне {{ p['age'] }} лет. Меня зовут {{ p['name']}}.")
+# msg = tm.render(p=per)
+# print(msg)
+
+# cities = [
+#     {'id': 1, 'city': 'Москва'},
+#     {'id': 2, 'city': 'Coчи'},
+#     {'id': 3, 'city': 'Москва'},
+#     {'id': 4, 'city': 'Смоленск'},
+#     {'id': 5, 'city': 'Ярославль'},
+#
+# ]
+# link = """
+# <select name="cities">
+# {% for c in cities -%}
+# <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+# {% endfor -%}
+# </select>
+# """
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
+
+# cities = [
+#     {'id': 1, 'city': 'Москва'},
+#     {'id': 2, 'city': 'Coчи'},
+#     {'id': 3, 'city': 'Москва'},
+#     {'id': 4, 'city': 'Смоленск'},
+#     {'id': 5, 'city': 'Ярославль'},
+#
+# ]
+# link = """
+# <select name="cities">
+# {% for c in cities -%}
+# {% if c.id > 3 -%}
+#     <option value="{{ c['id'] }}">{{ c['city'] }}</option>
+# {% elif c.city == 'Москва' -%}
+# {% else -%}
+#     {{ c['city'] }}
+# {% endif -%}
+# {% endfor -%}
+# </select>
+# """
+# tm = Template(link)
+# msg = tm.render(cities=cities)
+#
+# print(msg)
+
+car = [
+    {'model': 'Audi', 'price': 23000},
+    {'model': 'Skoda', 'price': 17300},
+    {'model': 'Renault', 'price': 44200},
+    {'model': 'Wolksvagen', 'price': 21300},
+
+]
+lst = [1, 2, 3, 4, 5, 6]
+# tpl = "{{ cs | sum(attribute = 'price') }}"
+tpl = "{{ cs | sum }}"
+tm = Template(tpl)
+# msg = tm.render(cs=car)
+msg = tm.render(cs=lst)
+
+print(msg)
